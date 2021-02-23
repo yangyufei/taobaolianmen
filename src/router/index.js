1c326fecfbce2bce41d4716e1c004b7e808afd79
yangyufei <316067641@qq.com>
2021-02-23 16:17:02 +0800

    '优化的登录页（之后要改成不优化的）'

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/login'
 
 const routes = [
   {
     path: '/',
     name: 'login',
-    component: Login
+    component: () => import('../views/login')
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = createRouter({
--- vue.config.js
+++ vue.config.js
@@ -1,41 +1,25 @@
-// const path = require('path')
-
-// const webpack = require('webpack')
-
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-
-// module.exports = {
-//   lintOnSave: false,
-//   chainWebpack: (config) => {
-//     config.resolve.alias
-//       .set('components', path.resolve(__dirname, 'src/components'))
-//       .set('common', path.resolve(__dirname, 'src/common'))
-//       .set('views', path.resolve(__dirname, 'src/views'))
-
-//   },
-//   devServer: {
-//     // 监听端口
-//     port: 8888
-//   },
-//   configureWebpack: {
-//     resolve: {
-//       alias: {
-//         '@': path.resolve(__dirname, 'src'),
-//       },
-//     },
-//     externals: {
-//       'vue': 'Vue',
-//       'vuex': 'Vuex',
-//       'axios': 'axios',
-//       'jquery': '$',
-//       'echarts': 'echarts',
-//     },
-//     plugins: [
-//       new webpack.ProvidePlugin({
-//         $: 'jquery',
-//         jQuery: 'jquery',
-//         'windows.jQuery': 'jquery'
-//       })
-//     ]
-//   }
-// }
+const path = require('path')
+const webpack = require('webpack')
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+module.exports = {
+  chainWebpack: config => {
+    //路径配置
+    config.resolve.alias
+      .set('@', resolve('src'))
+  },
+  devServer: {
+    // 调试端口
+    port: 8888
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "windows.jQuery": "jquery"
+      })
+    ]
+  }
+}
