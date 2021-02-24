c0a7661b2c63ee1f5b063616d10988bf93a5872a
yangyufei <316067641@qq.com>
2021-02-24 16:14:27 +0800

    '打包登录页'

--- vue.config.js
+++ vue.config.js
@@ -24,5 +24,5 @@ module.exports = {
   },
   // 生产环境 sourceMap
   productionSourceMap: false,
-  publicPath: process.env.NODE_ENV === 'production' ? './'
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
 }
