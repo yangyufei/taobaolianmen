4d70210c8d1ccf669d25150133ab9d495c034575
yangyufei <316067641@qq.com>
2021-02-26 11:33:34 +0800

    'app.vue页面误改的撤销'

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
   <div id="app">
     <!-- 使用userData来判断是否是登录页还是内页，userData有值说明是内页，则添加导航 -->
     <!-- <span v-if="userData">123456</span> -->
-    <layout v-if="$common.getSession('userInfo')"></layout>
+    <layout v-if="userData"></layout>
     <router-view />
   </div>
 </template>
@@ -21,9 +21,7 @@ export default {
     ...mapState('loginInfo', ['userData'])
   },
   watch: {
-    userData(val) {
-      this.$common.setSession('userInfo', val)
-    }
+
   }
 }
 </script>
