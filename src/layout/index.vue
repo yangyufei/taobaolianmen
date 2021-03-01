f6f1d4033e467db3dce8e62c55bd29083e917ada
yangyufei <316067641@qq.com>
2021-03-01 10:56:42 +0800

    '导航模块'

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -4,6 +4,7 @@
       <img :src="require('@/assets/img/common/index_logo.png')"
            alt="">
       <span class="toOldVersion">返回旧版</span>
+      <!-- TODO:需要先引入element-ui -->
       <navMenu></navMenu>
     </header>
     <router-view></router-view>
@@ -45,7 +46,8 @@ export default {
   header {
     position: relative;
     height: 60px;
-    padding-left: 80px;
+    padding-left: 286px;
+    box-sizing: border-box;
     box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
     img {
       position: absolute;
@@ -57,7 +59,7 @@ export default {
     .toOldVersion {
       position: absolute;
       top: 21px;
-      left: 164px;
+      left: 218px;
       background: #fff;
       border: 1px solid #e2e2e9;
       border-radius: 2px;
