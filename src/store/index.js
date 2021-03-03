b3c71f7d9b46c47e022329e12f0d185de5bff497
yangyufei <316067641@qq.com>
2021-03-03 10:12:42 +0800

    'store添加modules'

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
 import { createStore } from 'vuex'
 import preModules from './preModules'
 import createPersistedState from 'vuex-persistedstate'
-// const modulesFiles = require.context('./modules', true, /\.js$/)
-// const modules = modulesFiles.keys().reduce((modules, modulePath) => {
-//   // set './app.js' => 'app'
-//   const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
-//   const value = modulesFiles(modulePath)
-//   modules[moduleName] = value.default
-//   return modules
-// }, {})
+const modulesFiles = require.context('./modules', true, /\.js$/)
+const modules = modulesFiles.keys().reduce((modules, modulePath) => {
+  // set './app.js' => 'app'
+  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
+  const value = modulesFiles(modulePath)
+  modules[moduleName] = value.default
+  return modules
+}, {})
 
 export default createStore({
   state: {
@@ -18,7 +18,7 @@ export default createStore({
   actions: {
   },
   modules: {
-    // ...modules
+    ...modules,
     ...preModules
   },
   plugins: [
---
+++ src/store/modules/user.js
@@ -0,0 +1,21 @@
+const state = {
+  accout: 1
+}
+const mutations = {
+  SET_ACCOUNT(state, val) {
+    state.account = val
+  }
+}
+const actions = {
+
+}
+const getters = {
+
+}
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
+}
